713336d8fc82cac0f704dfbb562dba9461efe968
Wyatt-Gold <wyatt.golden123@icloud.com>
2022-10-31 14:28:21 -0500

    asdf
    asdf

--- Barnhouse Brawl/Animal.java
+++ Barnhouse Brawl/Animal.java
@@ -11,16 +11,39 @@ public class Animal extends Actor {
         CHICKEN,
         PIG
     }
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
 
     public void act() {
-        // Add your action code here.
+        if(playerID==1){
+            //Player 1 controls
+            if(Greenfoot.isKeyDown("W")){
+                movement(Direction.UP);
+            }
+            if(Greenfoot.isKeyDown("A")){
+                movement(Direction.LEFT);
+            }
+            if(Greenfoot.isKeyDown("S")){
+                movement(Direction.DOWN);
+            }
+            if(Greenfoot.isKeyDown("D")){
+                movement(Direction.RIGHT);
+            }
+        }
+        if(playerID==2){
+            //Player 2 controls
+        }
     }
 
     public void basicPush() {
         // Universal for all characters
     }
 
-    public void movement() {
+    public void movement(Direction direction) {
         // Physics: The heavier the slower you are
     }
 }
