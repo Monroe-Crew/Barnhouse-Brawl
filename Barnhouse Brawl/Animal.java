f0ac32494338b24e579c7ffd3025eeaad2e417b1
Wyatt-Gold <wyatt.golden123@icloud.com>
2022-10-31 14:17:24 -0500

    sdfg
    dsfg

--- Barnhouse Brawl/Animal.java
+++ Barnhouse Brawl/Animal.java
@@ -1,19 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Animal here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Animal extends Actor
-{
-    /**
-     * Act - do whatever the Animal wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
+public class Animal extends Actor{
+    private int weight;
+    private int playerID;
+    public Animal(int weight, int playerID){
+    }
+    public void act(){
         // Add your action code here.
-    }    
+    } 
+    public void basicPush(){
+        //Universal for all characters
+    }
+    public void movement(){
+        //Physics: The heavier the slower you are
+    }
 }
