adf50f79ac69ca76194e27f1b18d03e7d8f62498
Murad <agariomadman@gmail.com>
2022-10-31 14:25:27 -0500

    Animal class constructors

--- Barnhouse Brawl/Chicken.java
+++ Barnhouse Brawl/Chicken.java
@@ -1,17 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Chicken here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Chicken extends Animal
 {
-    /**
-     * Act - do whatever the Chicken wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    public Chicken(int playerID){
+        super(playerID, 1);
+    }
+    
     public void act() 
     {
         // Add your action code here.
--- Barnhouse Brawl/Pig.java
+++ Barnhouse Brawl/Pig.java
@@ -1,17 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Pig here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Pig extends Animal
 {
-    /**
-     * Act - do whatever the Pig wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    public Pig(int playerID){
+        super(playerID, 3);
+    }
+    
     public void act() 
     {
         // Add your action code here.
