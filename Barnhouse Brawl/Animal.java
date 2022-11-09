12d701b944e43702d89c65683e282f00135c7aac
Murad <agariomadman@gmail.com>
2022-11-09 14:40:16 -0600

    Stopped printing

--- Barnhouse Brawl/Animal.java
+++ Barnhouse Brawl/Animal.java
@@ -84,7 +84,6 @@ public class Animal extends Actor {
         ArrayList<Animal> touching = hitBox.findTouching();
         for(Animal animal : touching){
             if(animal == this) continue;
-            System.out.println((animal.getX() - this.getX() > 0 ? 1 : -1));
             int xStrength = (int)(((animal.getX() - this.getX() > 0 ? 1 : -1) * (double)animal.getX()/this.getX() * 5)/Math.sqrt(animal.getWeight()))*multiplier;
             int yStrength = (int)(((animal.getY() - this.getY() > 0 ? 1 : -1) * (double)animal.getY()/this.getY() * 5)/Math.sqrt(animal.getWeight()))*multiplier;
             animal.knockBack(xStrength, yStrength);
