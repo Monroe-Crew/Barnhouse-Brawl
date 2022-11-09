1503589a6ade802a35064f2e96f81989e1f99007
Wyatt-Gold <wyatt.golden123@icloud.com>
2022-11-09 16:28:38 -0600

    Added debug mode

--- Barnhouse Brawl/Hurtbox.java
+++ Barnhouse Brawl/Hurtbox.java
@@ -6,21 +6,26 @@ public class Hurtbox extends Actor
         this.actor = actor;
         int w = actor.getImage().getWidth()-5;
         int h = actor.getImage().getHeight()-1;
+
         GreenfootImage actorImage = actor.getImage();
-        GreenfootImage hurtboxImage = new GreenfootImage(w,h);//,);
-        hurtboxImage.setColor(new Color(0,0,255,127));
-        hurtboxImage.fillRect(0,0,w,h);
+        GreenfootImage hurtboxImage = new GreenfootImage(w,h);
+        if(Constants.debugMode){
+            hurtboxImage.setColor(new Color(0,0,255,127));
+            hurtboxImage.fillRect(0,0,w,h);
+        }
         setImage(hurtboxImage);
     }
+
     public void act() 
     {
         setLocation(actor.getX(), actor.getY());
         setRotation(actor.getRotation());
     }  
+
     public Actor getActor(){
         return this.actor;
     }
-    
+
     public void addedToWorld(World world) {
         setLocation(actor.getX(), actor.getY());
     }
