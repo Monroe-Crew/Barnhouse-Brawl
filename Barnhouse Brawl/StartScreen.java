99b54a0fce31e15aeae701f14513a352380d65e9
Murad <agariomadman@gmail.com>
2022-11-14 14:40:50 -0600

    Updated resolutions

--- Barnhouse Brawl/StartScreen.java
+++ Barnhouse Brawl/StartScreen.java
@@ -8,6 +8,6 @@ public class StartScreen extends World
     public StartScreen()
     {    
         
-        super(1280, 960, 1); 
+        super(Constants.Settings.worldWidth, Constants.Settings.worldHeight, 1);
     }
 }
