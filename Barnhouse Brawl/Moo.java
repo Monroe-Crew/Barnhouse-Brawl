92d39c102893a60b7593e0f871a8324d86578f76
Saugage <88468124+Saugage@users.noreply.github.com>
2022-12-02 14:34:23 -0600

    Colored BG and Better Moo!

--- Barnhouse Brawl/Moo.java
+++ Barnhouse Brawl/Moo.java
@@ -1,26 +1,29 @@
 import greenfoot.*; 
+
 public class Moo extends Actor
 {
     GreenfootImage moo;
-    int width;
-    int height;
+    GifImage gif = new GifImage("MooTest.gif");
+    //int width;
+    //int height;
     int time = 0;
     public Moo(){
-        moo = new GreenfootImage(getImage());
-        getImage().scale(128,32);
-        width = getImage().getWidth();
-        height = getImage().getHeight();
+        //gif = new GifImage(getImage());
+        //getImage().scale(128,32);
+        //width = getImage().getWidth();
+        //height = getImage().getHeight();
     }
 
     public void act() 
     {
         time++;
-        GreenfootImage placeholder = new GreenfootImage(moo);
-        double tempW = 128.0;
-        double tempH = 32.0;
-        placeholder.scale((int)tempW, (int)tempH);
-        setImage(placeholder);
-        if(time>100){
+        setImage(gif.getCurrentImage());
+        //GreenfootImage placeholder = new GreenfootImage(moo);
+        //double tempW = 128.0;
+        //double tempH = 32.0;
+        //placeholder.scale((int)tempW, (int)tempH);
+        //setImage(placeholder);
+        if(time>80){
             getWorld().removeObject(this);
         }
     }    
