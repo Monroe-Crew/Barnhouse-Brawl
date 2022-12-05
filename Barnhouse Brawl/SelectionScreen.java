f8e1dc2142b6543cfd72440b52a68d51fe2f88b1
Murad <agariomadman@gmail.com>
2022-12-05 13:54:28 -0600

    Update SelectionScreen.java

--- Barnhouse Brawl/SelectionScreen.java
+++ Barnhouse Brawl/SelectionScreen.java
@@ -27,7 +27,6 @@ public class SelectionScreen extends World {
     public SelectionScreen() {
         super(Constants.Settings.worldWidth, Constants.Settings.worldHeight, 1);
         AllPlayers.reset();
-        prepare();
     }
 
     public void act() {
@@ -72,17 +71,5 @@ public class SelectionScreen extends World {
 
     }
 
-    private void prepare() {
-        SelectorBox selectorBox1 = new SelectorBox(1);
-        addObject(selectorBox1, 170, 655);
 
-        SelectorBox selectorBox2 = new SelectorBox(2);
-        addObject(selectorBox2, 483, 655);
-
-        SelectorBox selectorBox3 = new SelectorBox(3);
-        addObject(selectorBox3, 794, 655);
-
-        SelectorBox selectorBox4 = new SelectorBox(4);
-        addObject(selectorBox4, 1106, 655);
-    }
 }
