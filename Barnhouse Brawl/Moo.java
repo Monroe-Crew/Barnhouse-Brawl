6c6321a36fc28f9688342244ec786c0b529fc4bf
Saugage <88468124+Saugage@users.noreply.github.com>
2022-12-08 15:23:45 -0600

    Stuff

--- Barnhouse Brawl/Moo.java
+++ Barnhouse Brawl/Moo.java
@@ -2,27 +2,16 @@ import greenfoot.*;
 
 public class Moo extends Actor
 {
-    GreenfootImage moo;
-    //GifImage gif = new GifImage("MooTest.gif");
-    //int width;
-    //int height;
+    //GreenfootImage moo;
+    GifImage gif = new GifImage("MooTest.gif");
     int time = 0;
     public Moo(){
-        //gif = new GifImage(getImage());
-        //getImage().scale(128,32);
-        //width = getImage().getWidth();
-        //height = getImage().getHeight();
     }
 
     public void act() 
     {
         time++;
-        //setImage(gif.getCurrentImage());
-        //GreenfootImage placeholder = new GreenfootImage(moo);
-        //double tempW = 128.0;
-        //double tempH = 32.0;
-        //placeholder.scale((int)tempW, (int)tempH);
-        //setImage(placeholder);
+        setImage(gif.getCurrentImage());
         if(time>80){
             getWorld().removeObject(this);
         }
